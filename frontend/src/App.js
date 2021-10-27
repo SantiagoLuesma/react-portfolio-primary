63041ea1a08e0c4d9af11b3589140ea173291bf1
Luesma <seluesma@gmail.com>
2021-10-27 00:09:56 -0300

    build(frontend): Creo la sección Home
    En este commit creo la sección Home en la página de inicio

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,12 @@
 import React, { Fragment } from 'react';
+import Main from './components/Main';
 import MenuNav from './components/MenuNav'
 
 export default function App() {
   return (
     <>
       <MenuNav />
+      <Main />
     </>
   )
 }
---
+++ frontend/src/components/Main.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import HomeSection from './HomeSection'
+
+export default function Main() {
+  return (
+    <>
+      <HomeSection />
+    </>
+  )
+}
